fb8cc9900f1f119c9a35c13a83d0af89e9eb3d60
Selina Schindlauer <selinaschindlauer@gmail.com>
2021-12-28 13:01:30 +0100

    chore: update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: "",
+  // purge: {
+  //   content: ["./src/**/*.{html,ts}"],
+  // },
+  content: ["./src/**/*.html" /* ... */],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
