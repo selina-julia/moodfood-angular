40b34215ec560e5c017308e3eaed80ae838d1f2e
Selina Schindlauer <selinaschindlauer@gmail.com>
2021-12-28 13:55:45 +0100

    chore: update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDXue2oc-BnS6sH1wgpwgdfztvNF15Bqhs',
+    authDomain: 'moodfood-ef2ef.firebaseapp.com',
+    projectId: 'moodfood-ef2ef',
+    storageBucket: 'moodfood-ef2ef.appspot.com',
+    messagingSenderId: '676357195466',
+    appId: '1:676357195466:web:964da455eb7e0a80bb1633',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDXue2oc-BnS6sH1wgpwgdfztvNF15Bqhs',
+    authDomain: 'moodfood-ef2ef.firebaseapp.com',
+    projectId: 'moodfood-ef2ef',
+    storageBucket: 'moodfood-ef2ef.appspot.com',
+    messagingSenderId: '676357195466',
+    appId: '1:676357195466:web:964da455eb7e0a80bb1633',
+  },
 };
 
 /*
